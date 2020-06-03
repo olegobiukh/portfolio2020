99f239e91a53c659f47123d162fade1ce9f82606
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-06-03 21:03:56 +0300

    core code

---
+++ build/precache-manifest.f124a726859b8c72509927303029ff47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8604e0b27e840e419aad2a68492e1800",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "0b556ba6ab0517aebc0d",
+    "url": "/portfolio2020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "511a886300a52f0767e1",
+    "url": "/portfolio2020/static/js/2.fed4292e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio2020/static/js/2.fed4292e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b556ba6ab0517aebc0d",
+    "url": "/portfolio2020/static/js/main.8400bbac.chunk.js"
+  },
+  {
+    "revision": "2f2f9552a859c52bd04e",
+    "url": "/portfolio2020/static/js/runtime-main.626836e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
